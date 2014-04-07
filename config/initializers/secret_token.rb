4f0049dad7023a52c88f035f35d92cff8d315888
daifu <daifu.ye@gmail.com>
2014-04-06 20:06:52 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EMCC::Application.config.secret_token = '8878503f80084dd2918654e3927ae324b7c21c22c3eb9b70993eb484ce59c43d1adac3a4ead10a26bb208ce3c743832a3c4c27f3d13e538681f6d0bcd2736a3d'
